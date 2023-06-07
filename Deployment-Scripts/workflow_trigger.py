d77e2a4680b361051944e8d1a8ac7fd82f5ce883
Suheab1995 <124862610+Suheab1995@users.noreply.github.com>
2023-06-07 19:26:07 +0530

    Update workflow_trigger.py

--- Deployment-Scripts/workflow_trigger.py
+++ Deployment-Scripts/workflow_trigger.py
@@ -7,8 +7,8 @@ TOKEN= str(sys.argv[1])
 OWNER= str(sys.argv[2])
 REPO= str(sys.argv[3])
 Workflow_Name= str(sys.argv[4])
-pl_Baseline_Number= str(sys.argv[5])
-pl_Baseline_Revision = str(sys.argv[6])
+# pl_Baseline_Number= str(sys.argv[5])
+# pl_Baseline_Revision = str(sys.argv[6])
 
 
 print( "the toke value is")
@@ -20,14 +20,14 @@ def trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision):
       }
 
       data = {
-        "event_type": Workflow_Name,
-        "client_payload": {
-          'baselinetag': pl_Baseline_Number,
-          'revision_number': pl_Baseline_Revision
+        "event_type": Workflow_Name
+       # "client_payload": {
+         # 'baselinetag': pl_Baseline_Number,
+          #'revision_number': pl_Baseline_Revision
         }
       }
 
       responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
       print("The respoinse message is ",responsevalue.content)
 
-trigger_workflow(Workflow_Name,pl_Baseline_Number,pl_Baseline_Revision)
+trigger_workflow(Workflow_Name)
